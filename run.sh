e507e47304669c038894e26025c636443c8647db
feegloo <olek.figiel@gmail.com>
2022-04-03 09:53:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
 echo "macOS bootstrap"
+
+# https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# https://github.com/artginzburg/MiddleClick-BigSur
+brew install --cask --no-quarantine middleclick
+
+# Make it open automatically when you log in
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/MiddleClick.app", hidden:true}'
