67d1f5dc69726b1f8e36e37f6b3f833ef61732bb
feegloo <olek.figiel@gmail.com>
2022-09-17 01:15:41 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,21 +7,12 @@ chsh -s /bin/bash
 # terminal - hide 'last login'
 touch ~/.hushlogin
 
+# terminal - hide 'The default interactive shell is now zsh.'
+touch ~/.bash_profile && echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_profile
+
 # install homebrew
 # https://brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install vim
 brew install vim
-
-# install middleclick (tap 3 fingers to open tabs)
-# https://github.com/artginzburg/MiddleClick-BigSur
-brew install --cask --no-quarantine middleclick
-# Make it open automatically when you log in
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/MiddleClick.app", hidden:true}'
-
-# mute macOS startup "chime" sound
-# https://github.com/matteoacrossi/nobootsound
-
-# disable the automatic creation of .DS_Store file
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true 
