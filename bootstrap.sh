54a100fe4bc9716391fc9386879d471f1b9a5d93
feegloo <olek.figiel@gmail.com>
2022-09-29 14:08:58 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,6 @@ chsh -s /bin/bash
 # terminal - hide 'last login'
 touch ~/.hushlogin
 
-# terminal - hide 'The default interactive shell is now zsh.'
-touch ~/.bash_profile && echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_profile
-
 # install homebrew
 # https://brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
